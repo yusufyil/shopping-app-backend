40ea4d8152e59fadb6d0a9dd00b3251c68c3d1cc
Yusuf Yildirim <yusufyildirim1025@gmail.com>
2022-10-30 23:56:58 +0300

    First blodd

---
+++ src/main/java/edu/marmara/shoppingappbackend/ShoppingAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package edu.marmara.shoppingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingAppBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/marmara/shoppingappbackend/ShoppingAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.marmara.shoppingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
